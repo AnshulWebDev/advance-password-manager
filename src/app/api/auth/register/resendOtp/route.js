536f38def10c5c8eb3187cd9337353cb6d3e020c
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-12-08 10:18:07 +0530

    Update route.js

--- src/app/api/auth/register/resendOtp/route.js
+++ src/app/api/auth/register/resendOtp/route.js
@@ -10,14 +10,14 @@ export const POST = async (req) => {
     if (!cookiesValue) {
       return NextResponse.json(
         { success: false, message: "Please register first" },
-        { status: 402 }
+        { status: 422 }
       );
     }
     const decode = Jwt.verify(cookiesValue, process.env.JWT_SECRET);
     if (!decode) {
       return NextResponse.json(
         { success: false, message: "unable to verify" },
-        { status: 406 }
+        { status: 401 }
       );
     }
     //generate 8 digit otp
@@ -27,7 +27,7 @@ export const POST = async (req) => {
       upperCaseAlphabets: false,
       specialChars: false,
     });
-
+    await connectDB();
     //create generated otp in OTP schema
     await otp.create({
       email: decode.email,
@@ -36,18 +36,83 @@ export const POST = async (req) => {
 
     //send otp via mail
     await mailSender(
-      email,
+      decode.email,
       "Verify Your Email",
-      `Verification code
-  
-        Please use the verification code below to sign in.
-        
-        ${OTP}
-        
-        If you didn’t request this, you can ignore this email.
-        
-        Thanks,
-        The DevGlimpse team`
+      `
+      <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Email Verification</title>
+  <style>
+    /* Add your styles here */
+    body {
+      font-family: 'Arial', sans-serif;
+      background-color: #f4f4f4;
+      margin: 0;
+      padding: 0;
+    }
+
+    .container {
+      max-width: 600px;
+      margin: 20px auto;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    h1 {
+      color: #262626;
+    }
+
+    p {
+      font-size: 16px;
+      line-height: 1.5;
+      color: #262626;
+    }
+
+    .otp {
+      display: inline-block;
+      padding: 8px 16px;
+      font-size: 18px;
+      font-weight: bold;
+      background-color: #1a82e2;
+      color: #fff;
+      border-radius: 4px;
+    }
+
+    .footer {
+      border-top: 2px solid #ccc;
+      padding-top: 20px;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+
+<body>
+
+  <div class="container">
+    <h1>Email Verification</h1>
+    <p>Thank you for signing up with CipherGuard. To complete your registration, please use the following One-Time Password (OTP):</p>
+
+    <p class="otp">${OTP}</p>
+
+    <p>This OTP is valid for 15 minutes. Do not share it with anyone for security reasons.</p>
+    <p>If you did not sign up for CipherGuard, please ignore this email.</p>
+
+    <div class="footer">
+      <p>Best,<br><a href="https://devglimpse.com" target="_blank">The CipherGuard team</a></p>
+    </div>
+  </div>
+
+</body>
+
+</html>
+
+      `
     );
     return NextResponse.json(
       { success: true, message: "OTP send successfully" },
@@ -55,15 +120,15 @@ export const POST = async (req) => {
     );
   } catch (error) {
     console.log(error.message);
-    if (
-      error.message === "Body is unusable" ||
-      "Unexpected end of JSON input"
-    ) {
-      return NextResponse.json(
-        { success: false, message: "Data can't be empty" },
-        { status: 406 }
-      );
-    }
+    // if (
+    //   error.message === "Body is unusable" ||
+    //   "Unexpected end of JSON input"
+    // ) {
+    //   return NextResponse.json(
+    //     { success: false, message: "Data can't be empty" },
+    //     { status: 406 }
+    //   );
+    // }
     return NextResponse.json(
       { success: false, message: "Internal server error Try Again" },
       { status: 500 }
