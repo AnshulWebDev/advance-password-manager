04807ea5503ac58e7ca4e11dd281ff81208f0ef2
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-12-11 12:20:35 +0530

    Update route.js

--- src/app/api/getPasswd/route.js
+++ src/app/api/getPasswd/route.js
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import { uniquepasswd } from "../../../../model/uniquepasswd";
 import { connectDB } from "../../../../utils/dbconnect";
 
@@ -36,8 +37,67 @@ export const POST = async (req) => {
   try {
     await connectDB();
     const { capital, small, special, number, length } = await req.json();
-    const token = await req.cookies.get("token")?.value;
+    if (!capital && !small && !special && !number) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Choose at least one option",
+        },
+        { status: 422 }
+      );
+    } else if (typeof length !== "number") {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Choose a valid password length",
+        },
+        { status: 422 }
+      );
+    } else if (
+      capital !== "capital" &&
+      small !== "small" &&
+      special !== "special" &&
+      number !== "number"
+    ) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Choose a correct password type",
+        },
+        { status: 422 }
+      );
+    } else if (length < 5 || length > 128) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Length should be between 5 to 128",
+        },
+        { status: 422 }
+      );
+    }
+
     const passwdDB = await uniquepasswd.find();
+    let generatedPassword;
+    do {
+      generatedPassword = uniquePassWdGenerator(
+        `${capital}+${small}+${special}+${number}`,
+        length
+      );
+    } while (passwdDB.some((entry) => entry.passwd === generatedPassword));
+
+    const newUniquePasswordEntry = new uniquepasswd({
+      passwd: generatedPassword,
+    });
+
+    await newUniquePasswordEntry.save();
+    return NextResponse.json(
+      {
+        success: true,
+        message: "password generated",
+        data: generatedPassword,
+      },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json(
       { success: false, message: "Internal server error Try Again" },
