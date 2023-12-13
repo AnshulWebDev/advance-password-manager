82bf08500dcc40c32a716e3972620e1013093a42
Anshul Vishwakarma <72655193+AnshulWebDev@users.noreply.github.com>
2023-12-13 23:13:21 +0530

    Update route.js

--- src/app/api/note/searchNote/route.js
+++ src/app/api/note/searchNote/route.js
@@ -1,13 +1,31 @@
 import { NextResponse } from "next/server";
+import { user as User } from "../../../../../model/user";
 
 export const GET = async (req) => {
   try {
+    const token = await req.cookies.get("token")?.value;
     const url = new URL(req.url);
     const name = url.searchParams.get("name");
-
-    console.log(name);
+    const user = await User.findOne({ token }).populate("secureNotes");
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "session expire Login again",
+        },
+        { status: 404 }
+      );
+    }
+    const searchResult = user.secureNotes.filter((note) =>
+      new RegExp(name, "i").test(note.name)
+    );
+    user.secureNotes;
     return NextResponse.json(
-      { success: true, message: "Success" },
+      {
+        success: true,
+        message: "Success",
+        data: searchResult.length > 0 ? searchResult : "No result found",
+      },
       { status: 200 }
     );
   } catch (error) {
