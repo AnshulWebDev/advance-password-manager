470d0b69010874b9234516c53570e48b3c8c4ef5
AnshulWebDev <anshulvishwa20@gmail.com>
2023-12-19 18:39:40 +0530

    Update route.js

--- src/app/api/passwordVault/createPasswd/route.js
+++ src/app/api/passwordVault/createPasswd/route.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { user as User } from "../../../../../model/user";
 import { connectDB } from "../../../../../utils/dbconnect";
 import { passwordvault } from "../../../../../model/passwordVault";
-import bcrypt from "bcrypt";
 import CryptoJS from "crypto-js";
 export const POST = async (req) => {
   try {
@@ -23,6 +22,21 @@ export const POST = async (req) => {
         { success: false, message: "Enter all fields" },
         { status: 402 }
       );
+    } else if (!vaultPin || vaultPin.toString().length !== 6) {
+      return NextResponse.json(
+        { success: false, message: "Enter a 6-digit number vault pin" },
+        { status: 422 }
+      );
+    } else if (!user.vaultPin) {
+      return NextResponse.json(
+        { success: false, message: "create 6 digit vault pin" },
+        { status: 422 }
+      );
+    } else if (!(await bcrypt.compare(vaultPin, user.vaultPin))) {
+      return NextResponse.json(
+        { success: false, message: "Vault pin is incorrect" },
+        { status: 402 }
+      );
     }
     const currentDate = new Date().toLocaleString("en-US", {
       year: "numeric",
@@ -34,14 +48,14 @@ export const POST = async (req) => {
       hour12: true,
       timeZone: "Asia/Kolkata",
     });
-    const encryptNote = await CryptoJS.AES.encrypt(
+    const encryptPasswd = await CryptoJS.AES.encrypt(
       password,
-      vaultPin //todo
+      vaultPin
     ).toString();
     const newPassword = await passwordvault.create({
       name,
       username,
-      password: encryptNote,
+      password: encryptPasswd,
       website,
       Created: currentDate,
     });
