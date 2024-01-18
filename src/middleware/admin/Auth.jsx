51f308587c5c8e9eb85198d866d5e36e2dae4ee3
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-18 13:17:47 +0530

    Update Auth.jsx

--- src/middleware/admin/Auth.jsx
+++ src/middleware/admin/Auth.jsx
@@ -4,8 +4,9 @@ import { Navigate } from "react-router-dom";
 
 const Auth = ({ Component }) => {
   const cookies = new Cookies();
-  const token = cookies.get("admin_token") || localStorage.getItem("admin_token");
-  if (!token) {
+  const token = cookies.get("admin_token") || localStorage.getItem("admin_token"); 
+  const adminProfile = JSON.parse(localStorage.getItem("admin_profile"));
+  if (!token && adminProfile === null ) {
     // console.log("no token");
     return <Navigate to={"/admin/login"} />;
   } else {
