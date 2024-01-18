f98dead3d56052cbab33b2ea57133e88ce153a92
AnshulWebDev <anshulvishwa20@gmail.com>
2024-01-18 20:38:27 +0530

    Update Auth.jsx

--- src/middleware/admin/Auth.jsx
+++ src/middleware/admin/Auth.jsx
@@ -1,12 +1,48 @@
+import { useEffect } from "react";
 import { Cookies } from "react-cookie";
 import { isExpired, decodeToken } from "react-jwt";
 import { Navigate } from "react-router-dom";
-
-const Auth = ({ Component = () => null}) => {
+import axios from "axios";
+const Auth = ({ Component = () => null }) => {
   const cookies = new Cookies();
-  const token = cookies.get("admin_token") || localStorage.getItem("admin_token"); 
+  const token =
+    cookies.get("admin_token") || localStorage.getItem("admin_token");
   const adminProfile = JSON.parse(localStorage.getItem("admin_profile"));
-  if (!token && !adminProfile ) {
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      await axios
+        .post(
+          `${import.meta.env.VITE_BACKEND_URL}/api/admin/adminProfile`,
+          "",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .then(function (response) {
+          setData(response.data.data);
+          // console.log(response.data.data);
+        })
+        .catch(function (error) {
+          if (
+            error.response.data.message === "session expired, Login again" ||
+            error.response.data.message === "Token Is missing" ||
+            error.response.data.message === "Token Is Invalid"
+          ) {
+            cookies.remove("admin_token");
+            localStorage.removeItem("admin_token");
+            localStorage.removeItem("admin_profile");
+          }
+          // console.error(error.response.data.message);
+        });
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+  if (!token && !adminProfile) {
     // console.log("no token");
     return <Navigate to={"/admin/login"} />;
   } else {
